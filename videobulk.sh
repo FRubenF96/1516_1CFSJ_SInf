da3ff55e165e5e24009b6c8737e7ae00ef207b8a
josete96 <josesancheztor@gmail.com>
2016-04-29 13:47:46 +0200

    mychange

--- videobulk.sh
+++ videobulk.sh
@@ -6,24 +6,45 @@
 # ...
 
 # Main
-#Sanity checks
-if [ S# -eq 0 ]
-then
-directory=$PWD
-elif [ -d $1 ]
-then
-directory =$1
-else
-echo "sorry. $1 is not a directory"
-fi code
+#!/bin/bash
+echo "1) Transforma video"
+echo "2) Renombrar video"
+
+echo "Elija opcion:"
+read opcion
+case $opcion in
+
+1 ) if [ $# -eq 0 ]
+	then
+	directory=$PWD
+	elif [ -d $1 ]
+	then
+	directory =$1
+	else
+	echo "sorry. $1 is not a directory"
+fi 
 
 ls -1 $1*.avi > videofiles
 
-while videofile
-do
+while read videofile
+	do
 	lengthname=${#videofile}
-	namefile=${videofile;0;$lengthname-4}
-	mencoder $videofile -o namefile.mp4 -oac mp3lame -ovc lavc -of mp4
+	namefile=${videofile:-3}
+	mencoder $videofile -o namefile.mp4 -oac mp3lame -ovc lavc
 
 done < videofiles
 rm videofiles
+exit 0
+;;
+
+
+
+
+2 ) read Name
+Name=$Name".mp4"
+mv namefile.mp4 $Name
+exit 0
+;;
+
+* ) echo "Opciones de 1 a 2 solamente";;
+esac 
