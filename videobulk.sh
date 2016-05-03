256d7f7168e8c7c8a99a90dff959aa012877dd1b
FRubenF96 <frubenf96@gmail.com>
2016-05-03 13:01:01 +0200

    xe3

--- videobulk.sh
+++ videobulk.sh
@@ -1,9 +1,7 @@
 #!/bin/bash 
 # Main
-<<<<<<< HEAD
  #Sanity checks
--if [ S# -eq 0 ]
-+if [ $# -eq 0 ]
+if [ $# -eq 0 ]
  then
  directory=$PWD
  elif [ -d $1 ]
@@ -11,33 +9,6 @@
  directory =$1
  else
  echo "sorry. $1 is not a directory"
--fi code
--
-+fi 
-+echo "gggggggg"
- ls -1 $1*.avi > videofiles
- 
--while videofile
-+while read videofile
- do
- 	lengthname=${#videofile}
--	namefile=${videofile;0;$lengthname-4}
--	mencoder $videofile -o namefile.mp4 -oac mp3lame -ovc lavc -of mp4
-+	namefile=${videofile: -3}
-+	mencoder $videofile -o namefile.mp4 -oac mp3lame -ovc lavc
- 
- done < videofiles
- rm videofiles
-=======
-#Sanity checks
-if [ $# -eq 0 ]
-then
-directory=$PWD
-elif [ -d $1 ]
-then
-directory=$1
-else
-echo "sorry. $1 is not a directory"
 fi 
 
 Lengths=${#directory}
@@ -61,4 +32,3 @@ done < videofiles
 rm videofiles
 	
 exit 0
->>>>>>> 81c3158ebe988b85c76629a13a674502401d526c
