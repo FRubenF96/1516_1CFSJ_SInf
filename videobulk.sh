9678589d326a8facdc5aaf122bb54f37b8d23b26
hugo.vila96@gmail.com <hugo.vila96@gmail.com>
2016-05-03 12:45:26 +0200

    xe

--- videobulk.sh
+++ videobulk.sh
@@ -1,33 +1,29 @@
 #!/bin/bash 
-
-# Script to convert all video files from a directory (parameter) into mp4 format file
-
-# Sanity checks ?
-# ...
-
 # Main
-#Sanity checks
-
-# I used set-x and set+x to debug the code
-set -x
-if [ S# -eq 0 ]
-then
-directory=$PWD
-elif [ -d $1 ]
-then
-directory =$1
-else
-echo "sorry. $1 is not a directory"
-fi code
-
-ls -1 $1*.avi > videofiles
-
-while videofile
-do
-	lengthname=${#videofile}
-	namefile=${videofile;0;$lengthname-4}
-	mencoder $videofile -o namefile.mp4 -oac mp3lame -ovc lavc -of mp4
-
-done < videofiles
-set +x
-rm videofiles
+ #Sanity checks
+-if [ S# -eq 0 ]
++if [ $# -eq 0 ]
+ then
+ directory=$PWD
+ elif [ -d $1 ]
+ then
+ directory =$1
+ else
+ echo "sorry. $1 is not a directory"
+-fi code
+-
++fi 
++echo "gggggggg"
+ ls -1 $1*.avi > videofiles
+ 
+-while videofile
++while read videofile
+ do
+ 	lengthname=${#videofile}
+-	namefile=${videofile;0;$lengthname-4}
+-	mencoder $videofile -o namefile.mp4 -oac mp3lame -ovc lavc -of mp4
++	namefile=${videofile: -3}
++	mencoder $videofile -o namefile.mp4 -oac mp3lame -ovc lavc
+ 
+ done < videofiles
+ rm videofiles
